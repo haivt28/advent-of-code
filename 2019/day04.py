4d876bb22a5ca3dfcd91640890f82694d4961515
haivt <haivt2810@gmail.com>
2019-12-13 11:24:47 +0700

    Refactor day04

--- 2019/day04.py
+++ 2019/day04.py
@@ -1,8 +1,12 @@
 import math
 
 
-count = 0
-for i in range(130254, 678275):
+data = list(map(int, open('input/day04.txt').read().split('-')))
+
+# PART 1 + PART 2 can be merged
+count1 = 0
+count2 = 0
+for i in range(data[0], data[1]):
     i0 = i % 10
     i1 = math.floor(i/10) % 10
     i2 = math.floor(i/100) % 10
@@ -11,7 +15,9 @@ for i in range(130254, 678275):
     i5 = math.floor(i/100000) % 10
     if i0 == i1 or i1 == i2 or i2 == i3 or i3 == i4 or i4 == i5:
         if i0 >= i1 >= i2 >= i3 >= i4 >= i5:
+            count1 += 1
             if i0 == i1 != i2 or i0 != i1 == i2 != i3 or i1 != i2 == i3 != i4 or i2 != i3 == i4 != i5 or i3 != i4 == i5:
-                count += 1
+                count2 += 1
 
-print(count)
+print(count1)
+print(count2)
